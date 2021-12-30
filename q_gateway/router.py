228f054dc5f67d3fddb0c8cf77f0723817405229
Baldwin Sung <baldwinsung@users.noreply.github.com>
2021-12-29 20:16:11 -0500

    Updated - https - output from dnsserver - tidy up test code

--- q_gateway/router.py
+++ q_gateway/router.py
@@ -2,18 +2,23 @@ import requests
 import pprint
 import hashlib
 import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Sessions Number: No more than 5 sessions at a time are allowed. Please wait until open sessions expire.
+# Configurable from FIOS Admin Advanced Settings to 15
 
 
 class Router(object):
     _HEADERS = {"Host": "192.168.1.1",
                 "Connection": "keep-alive",
                 "Accept": "application/json, text/plain, */*",
-                "Origin": "http://192.168.1.1",
+                "Origin": "https://192.168.1.1",
                 "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
                 + "AppleWebKit/537.36 (KHTML, like Gecko) "
                 + "Chrome/64.0.3282.140 Safari/537.36",
                 "Content-Type": "application/json;charset=UTF-8",
-                "Referer": "http://192.168.1.1/",
+                "Referer": "https://192.168.1.1/",
                 "Accept-Encoding": "gzip, deflate",
                 "Accept-Language": "en-US,en;q=0.9"}
 
@@ -26,35 +31,38 @@ class Router(object):
         self.user = user
         self.password = password
         self.rsess = requests.Session()
-        self.api_base = 'http://{}/api'.format(ip)
-        get_api = self.rsess.get(self.api_base)
+        self.api_base = 'https://{}/api'.format(ip)
+        get_api = self.rsess.get(self.api_base, verify=False)
         self.api = get_api.json()
 
     def login(self):
-        # get salt first
-        pw_to_send = hashlib.sha512(bytes(self.password
-                                          + self.api['passwordSalt']
-                                          ).encode('utf-8')).hexdigest()
+        pre_pw_to_send = hashlib.sha512()
+        pre_pw_to_send.update(
+            (self.password + self.api['passwordSalt']).encode('utf-8'))
+        pw_to_send = pre_pw_to_send.hexdigest()
         payload = {"password": pw_to_send}
         clen, strpayload = Router.content_len_and_str(payload)
         Router._HEADERS.update({"Content-Length": str(clen)})
-        login_res = self.rsess.post(self.api_base + "/login",
-                                    data=strpayload,
-                                    headers=Router._HEADERS)
+        login_res = self.rsess.post(
+            self.api_base + "/login", data=strpayload, headers=Router._HEADERS)
         return login_res.status_code == 200
+
+    def logout(self):
+        self.rsess.get(self.api_base + '/api/logout', headers=Router._HEADERS)
+        self.rsess.close()
 
     def system(self):
         Router._HEADERS.update(
-                {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
+            {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
         Router._HEADERS.pop("Origin", None)
         Router._HEADERS.pop("Content-Length", None)
-        res = self.rsess.get(self.api_base + "/settings/system",
-                             headers=Router._HEADERS)
+        res = self.rsess.get(
+            self.api_base + "/settings/system", headers=Router._HEADERS)
         pprint.pprint(res.json())
 
     def firmware(self):
         Router._HEADERS.update(
-                {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
+            {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
         Router._HEADERS.pop("Origin", None)
         Router._HEADERS.pop("Content-Length", None)
         res = self.rsess.get(self.api_base + "/firmware",
@@ -63,7 +71,7 @@ class Router(object):
 
     def network(self):
         Router._HEADERS.update(
-                {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
+            {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
         Router._HEADERS.pop("Origin", None)
         Router._HEADERS.pop("Content-Length", None)
         res = self.rsess.get(self.api_base + "/network",
@@ -72,7 +80,7 @@ class Router(object):
 
     def devices(self):
         Router._HEADERS.update(
-                {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
+            {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
         Router._HEADERS.pop("Origin", None)
         Router._HEADERS.pop("Content-Length", None)
         res = self.rsess.get(self.api_base + "/devices",
@@ -81,16 +89,23 @@ class Router(object):
 
     def dnsserver(self):
         Router._HEADERS.update(
-                {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
+            {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
         Router._HEADERS.pop("Origin", None)
         Router._HEADERS.pop("Content-Length", None)
         res = self.rsess.get(self.api_base + "/settings/dnsserver",
                              headers=Router._HEADERS)
-        pprint.pprint(res.json())
+        # pprint.pprint(res.json())
+
+        jsonResponse = res.json()
+        jsonResponse.sort(key=lambda item: item.get("hostname"))
+        for i in jsonResponse:
+            hn = i['hostname']
+            ip = i['ipAddress']
+            print(hn+" "+ip)
 
     def portforward(self):
         Router._HEADERS.update(
-                {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
+            {"X-XSRF-TOKEN": self.rsess.cookies.get_dict()["XSRF-TOKEN"]})
         Router._HEADERS.pop("Origin", None)
         Router._HEADERS.pop("Content-Length", None)
         res = self.rsess.get(self.api_base + "/firewall/portforward",
